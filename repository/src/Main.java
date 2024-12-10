df127c3e872ad64e0411465582e98b1e3e08e2e8
bllirik <kirillsbozkovs2@gmail.com>
2024-12-10 09:28:40 +0200

    Add files via upload

---
+++ repository/src/Main.java
@@ -0,0 +1,39 @@
+
+//package stunda2;
+
+public class Main {
+
+    public static void main(String[] args) {
+sum(1, 2);
+
+
+
+
+    }
+
+    public static void sum(int a, int b){
+        int res = a+b;
+        System.out.println(res);
+    }
+
+    public static void better(){
+        System.out.println("Mix the dry ingredients.");
+        System.out.println("Cream the butter and sugar.");
+        System.out.println("Beat in the eggs.");
+        System.out.println("Stir in the dry ingredients.");
+    }
+    public static void bake(){
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+    }
+    public static void decorate (){
+        System.out.println("Mix ingredients for frosting.");
+        System.out.println("Spread frosting and sprinkles.");
+    }
+}
